121db84a03725b8e05b68b34bc851193a5732a8a
Paul <EdPaul1>
2024-07-29 11:06:29 +0300

    Creating an "Autobind" Decorator

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,24 @@
+//autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor){
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get(){
+            const boundFn = originalMethod.bing(this);
+            return boundFn;
+        }
+    }
+    return adjDescriptor;
+
+}
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
 
     constructor() {
@@ -11,7 +28,26 @@ class ProjectInput {
         //import the html content
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        //Get form inputs
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+        this.configure();
         this.attach();
+    }
+
+    @autobind
+    private submitHandler(event: Event) {
+        event.preventDefault()
+        console.log(this.titleInputElement.value)
+
+    }
+    // Event Listener for the form
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler)
     }
 
     private attach() {
